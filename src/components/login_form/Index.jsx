0bf3e53faf47ff67c8c9206bd29b23f87925da42
DylanC <88355961+comeaudc@users.noreply.github.com>
2024-07-31 13:31:35 -0400

    Working Commit

--- src/components/login_form/Index.jsx
+++ src/components/login_form/Index.jsx
@@ -1,4 +1,29 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/auth/auth_context';
+
 const LoginForm = ({ setNewUser }) => {
+  const nav = useNavigate();
+  const { login } = useAuth();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(formData);
+    console.log('Success');
+    nav('/dashboard');
+  };
+
   const handleClick = () => {
     setNewUser(true);
   };
@@ -6,9 +31,15 @@ const LoginForm = ({ setNewUser }) => {
   return (
     <div className='forms'>
       <h2>Login</h2>
-      <form autoComplete='off'>
+      <form onSubmit={handleSubmit} autoComplete='off'>
         <label htmlFor='email'>Email: </label>
-        <input type='email' id='email' name='email' placeholder='Email' />
+        <input
+          type='email'
+          id='email'
+          name='email'
+          onChange={handleChange}
+          placeholder='Email'
+        />
         <label htmlFor='password'>Password: </label>
         <input
           type='password'
@@ -16,10 +47,9 @@ const LoginForm = ({ setNewUser }) => {
           name='password'
           placeholder='Password'
           minLength='6'
+          onChange={handleChange}
         />
-        <button type='submit' onClick={handleClick}>
-          Log In
-        </button>
+        <button type='submit'>Log In</button>
       </form>
       <p>
         Dont have an account? <button onClick={handleClick}>Sign Up</button>
--- src/components/signup_form/Index.jsx
+++ src/components/signup_form/Index.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/auth/auth_context';
 
 const SignUp = ({ setNewUser }) => {
+  const nav = useNavigate();
+  const { signUp } = useAuth();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.password !== formData.password2) {
+        throw 'Password Do Not Match';
+      } else {
+        await signUp(formData);
+        nav('/dashboard');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const handleClick = () => {
     setNewUser(false);
   };
@@ -8,13 +40,14 @@ const SignUp = ({ setNewUser }) => {
   return (
     <div className='forms'>
       <h2>SignUp</h2>
-      <form autoComplete='off'>
+      <form onSubmit={handleSubmit} autoComplete='off'>
         <label htmlFor='name1'>Name: </label>
         <input
           type='text'
           id='name1'
           name='name'
           placeholder='First and Last Name'
+          onChange={handleChange}
         />
         <label htmlFor='email1'>Email: </label>
         <input
@@ -22,6 +55,7 @@ const SignUp = ({ setNewUser }) => {
           id='email1'
           name='email'
           placeholder='Email'
+          onChange={handleChange}
         />
         <label htmlFor='password1'>Password: </label>
         <input
@@ -30,6 +64,7 @@ const SignUp = ({ setNewUser }) => {
           name='password'
           placeholder='Password'
           minLength='6'
+          onChange={handleChange}
         />
         <input
           type='password'
@@ -37,6 +72,7 @@ const SignUp = ({ setNewUser }) => {
           name='password2'
           placeholder='Confirm Password'
           minLength='6'
+          onChange={handleChange}
         />
         <button type='submit'>Sign In</button>
       </form>
